e8a34801c04c16b6b7212a5377ea0bc102975a42
bryan_rodriguez_litardo1997 <bryan_rodriguez_litardo1997@hotmail.com>
2025-07-17 04:28:00 +0000

    Upload23

--- lib/servi_noti.dart
+++ lib/servi_noti.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'dart:io';
+//import 'package:permission_handler/permission_handler.dart';
+//import 'dart:io';
 
 class NotiService {
   final notificationsPlugin = FlutterLocalNotificationsPlugin();
